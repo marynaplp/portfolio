54fff4a0bf98a71d12c9c51034cb16eaed82d818
Maryna Pylypchenko <51541451+marynaplp@users.noreply.github.com>
2025-02-23 21:05:42 -0800

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
 	let data;
 	try {
 		data = await resend.emails.send({
-			from: "Contact Form <onboarding@resend.dev>",
+			from: "Contact Form <noreply@maryna-dev.ca>",
 			to: "maryna.pylypchenko@gmail.com",
 			subject: "Message from contact form",
 			reply_to: senderEmail as string,
